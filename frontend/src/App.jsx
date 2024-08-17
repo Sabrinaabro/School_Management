42dca9943b3bfef522d5175dcc189a1834a54ca9
Sabrinaabro <sabrinaazam123@gmail.com>
2024-08-17 17:29:54 +0500

    feat/form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
+import StudentForm from '../components/StudentForm';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/form" element={<StudentForm />} />
     </Routes>
   );
 }
